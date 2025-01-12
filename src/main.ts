91a605e6ac46e87d76ef074760b2aface7fb25f8
dasiloy <oyewoledamilare00@gmail.com>
2025-01-12 18:56:35 +0100

    testing ci/cd

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
       transport: Transport.TCP,
       options: {
         host: '0.0.0.0',
-        port: 4000,
+        port: 8000,
         retryAttempts: 5,
         retryDelay: 3000,
       },
